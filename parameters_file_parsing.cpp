7708206a5ee3ec0a255091def35e205aaaaa545c
qclayssen <quentin.clayssen@gmail.com>
2018-11-09 15:04:10 +0100

    liste parameters

--- parameters_file_parsing.cpp
+++ parameters_file_parsing.cpp
@@ -30,3 +30,55 @@ Parameters_file_parsing::Parameters_file_parsing()
         std::cerr << "Error while opening PARAMETERS_PROJECT.txt !\n";
     }
 }
+
+
+////////////////////////////////////////////////////////////
+
+
+
+void Parameters_file_parsing::import_line(string const& line)
+{
+    vector<string> token = this->split(line, ' ');
+    string const& key = token[0];
+    string & value = token[1];
+
+    if(key == "header")
+        header = atoi(value.c_str());
+
+    else if(key == "separator")
+    {
+        if(value == "\t")
+            separator = '\t';
+        else
+            separator = value.at(0);
+    }
+    else if(key == "k")
+
+        k = atof(value.c_str());
+
+    else if(key == "relinking_local_search_id")
+        relinking_local_search_id = atof(value.c_str());
+
+    else if(key == "n")
+        n = value;
+
+    else if(key == "memetique_local_search_id")
+        n_smmb_aco_runs = atoi(value.c_str());
+
+    else if(key == "n_it")
+        aco_n_ants = atoi(value.c_str());
+
+    else if(key == "n_pairs_selected_parents")
+        n_pairs_selected_parents = atoi(value.c_str());
+
+    else if(key == "prob_mutation")
+        prob_mutation = atoi(value.c_str());
+
+    else if(key == "best_k")
+        best_k = atoi(value.c_str());
+
+
+
+    else {}
+
+}
