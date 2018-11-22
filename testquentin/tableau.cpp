4a1aaf08361a5c8e5305217b58e0120b702cae9c
qclayssen <quentin.clayssen@gmail.com>
2018-11-22 14:37:01 +0100

    Gscore

--- testquentin/tableau.cpp
+++ testquentin/tableau.cpp
@@ -20,7 +20,7 @@ F mesure temps d'execution
 #include <stdlib.h>
 #include <ctime>
 #include <chrono>
-
+#include <boost/math/distributions/chi_squared.hpp>
 #include "CSVParser.hpp"
 #include "common.h"
 
@@ -29,190 +29,95 @@ using namespace std::chrono;
 
 
 
-/*float khi(int contingence[2][9] ,int nbrligne, int nbrcolonnes)
-{    for(int i(0); i<(nbrcolonnes-1); ++i)
-    {    for(int j(0); j<(nbrligne-1); ++j)
-        {
-           contingence[nbrligne][i] += contingence[i][j];
-        }
-    }
-
-
-    for(int j(0); j<(nbrcolonnes-1); ++j)
-    {    for(int i(0); i<(nbrligne-1); ++i)
-        {
-           contingence[j][nbrcolonnes] += contingence[i][j];
-        }
-    }
-
-
-
-    int contingencetheo[nbrcolonnes-1][nbrligne-1];
-    for(int i(0); i<(nbrcolonnes-1); ++i)
-    {    for(int j(0); j<(nbrligne-1); ++j)
-        {
-           contingencetheo[i][j] = contingence[i][nbrligne]*contingence[nbrcolonnes][j]/contingence[nbrcolonnes][nbrligne];
-        }
-    }
-
-
-
-    int scorekhi2=0;
-    for(int i(0); i<(nbrcolonnes-1); ++i)
-    {    for(int j(0); j<(nbrligne-1); ++j)
-        {
-           scorekhi2 += (contingence[i][j]-pow(contingencetheo[i][j],2))/contingence[nbrcolonnes][nbrligne];
-        }
-    }
-
-    return (scorekhi2);
-
-}*/
-
-
-
-int main()
+float khi2(int contingence[2][9] ,int nbrligne, int nbrcolonnes)
 {
-    // Arguments
-    string genos_file;
-    genos_file="simu1_Genotype_1.csv";
-    cout<<"Genotype file path : "<<endl;
-    string phenos_file;
-    phenos_file="simu1_Phenotype_1.csv";
-    cout<<"Phenotype file path : "<<endl;
-    int header = 1;
-    char separator = ',';
 
-//  DATA IMPORTATION
-    CSVParser<int> genos_csv(genos_file, separator, header);
-    CSVParser<int> phenos_csv(phenos_file, separator, header);
-    blas_matrix genos = genos_csv.data();
-    blas_matrix phenos_m = phenos_csv.data();
-    blas_column phenos(phenos_m, 0);
-    cout << endl << "Data imported : " << genos.size1() << " individuals X " << genos.size2() << " SNPs" << endl;
-    int k,l1,l2;
+for(int j(0); j<(nbrcolonnes); ++j)
+{    for(int i(0); i<(nbrligne); ++i)
+    {
 
-    int contingence[2][9]={0};
-    for (k=0;k<int(genos.size2())-1;k++){
-      contingence[2][9]={0};
-      for (l1=0;l1<int(genos.size1())-2;l1++){
-        for (l2=1;l2<int(genos.size1())-1;l2++){
-          if (phenos_m(l1,0)==1){
-            if (genos(l1,k)==0){
-              if (genos(l2,k)==0){
-                contingence[0][0]+=1;
-              }
-              else if (genos(l2,k)==1){
-                contingence[0][1]+=1;
-              }
-              else{
-                contingence[0][2]+=1;
-              }
-            }
-            else if (genos(l1,k)==0){
-              if (genos(l2,k)==0){
-                contingence[0][3]+=1;
-              }
-              else if (genos(l2,k)==1){
-                contingence[0][4]+=1;
-              }
-              else{
-                contingence[0][5]+=1;
-              }
-            }
-            else{
-              if (genos(l2,k)==0){
-                contingence[0][6]+=1;
-              }
-              else if (genos(l2,k)==1){
-                contingence[0][7]+=1;
-              }
-              else{
-                contingence[0][8]+=1;
-              }
-            }
-          }
-          else{
-            if (genos(l1,k)==0){
-              if (genos(l2,k)==0){
-                contingence[1][0]++;
-              }
-              else if (genos(l2,k)==1){
-                contingence[1][1]++;
-              }
-              else{
-                contingence[1][2]++;
-              }
-            }
-            else if (genos(l1,k)==0){
-              if (genos(l2,k)==0){
-                contingence[1][3]++;
-              }
-              else if (genos(l2,k)==1){
-                contingence[1][4]++;
-              }
-              else{
-                contingence[1][5]++;
-              }
-            }
-            else{
-              if (genos(l2,k)==0){
-                contingence[1][6]++;
-              }
-              else if (genos(l2,k)==1){
-                contingence[1][7]++;
-              }
-              else{
-                contingence[1][8]++;
-              }
-            }
-          }
-        }
-      }
-      for (int i=0;i<3;i++){
-        for (int j=0;j<10;j++){
-          cout<<contingence[j][i]<<" ";
-        }
-        cout<<endl;
+       cout<<contingence[nbrligne][j]<<"+"<<contingence[i][j]<<"=";
+       contingence[nbrligne][j] += contingence[i][j];
+       cout<<contingence[nbrligne][j]<<" ";
+
+    }cout<<";"<<endl;
+}
+for (int i=0;i<3;i++){
+  for (int j=0;j<10;j++){
+    cout<<contingence[i][j]<<" ";
+  }
+  cout<<endl;
+}
+cout<<endl;
+
+
+
+for(int j(0); j<(nbrcolonnes); ++j)
+{    for(int i(0); i<(nbrligne); ++i)
+    {
+       contingence[i][nbrcolonnes] += contingence[i][j];
+    }
+}
+for(int i(0); i<(nbrligne); ++i)
+    {
+       contingence[nbrligne][nbrcolonnes] += contingence[i][nbrcolonnes];
+    }
+for (int i=0;i<3;i++){
+  for (int j=0;j<10;j++){
+    cout<<contingence[i][j]<<" ";
+  }
+  cout<<endl;
+}
+cout<<endl;
+
+
+
+int contingencetheo[nbrcolonnes][nbrligne];
+for(int i(0); i<(nbrligne); ++i)
+{    for(int j(0); j<(nbrcolonnes); ++j)
+    {
+       cout<<contingence[i][nbrcolonnes]<<"*"<<contingence[nbrligne][j]<<"/"<<contingence[nbrligne][nbrcolonnes];
+       contingencetheo[i][j] = contingence[i][nbrcolonnes]*contingence[nbrligne][j]/contingence[nbrligne][nbrcolonnes];
+       cout<<"="<<contingencetheo[i][j]<<endl;
+    }
+}    for (int i=0;i<2;i++){
+      for (int j=0;j<9;j++){
+        cout<<contingencetheo[i][j]<<" ";
       }
       cout<<endl;
     }
-    int const nbrcolonnes(9);
-    int const nbrligne(2);
-    for(int i(0); i<(nbrcolonnes-1); ++i)
-    {    for(int j(0); j<(nbrligne-1); ++j)
+    cout<<endl;
+
+
+
+float scorekhi2=0;
+for(int i(0); i<(nbrligne); ++i)
+{    for(int j(0); j<(nbrcolonnes); ++j)
+    {
+       scorekhi2 += (pow(((contingence[i][j]-(contingencetheo[i][j]))),2)/contingencetheo[i][j]);
+       cout<<scorekhi2<<"="<<contingence[i][j]<<"-"<<contingencetheo[i][j]<<"^2"<<"/"<<contingencetheo[i][j]<<endl;
+    }
+}
+
+float scorekhi2=0;
+for(int i(0); i<(nbrligne); ++i)
+{    for(int j(0); j<(nbrcolonnes); ++j)
+    {
+        if(c(i,j) != 0)
         {
-           contingence[nbrligne][i] += contingence[i][j];
+            double div = (double) contingence[i][j] / contingencetheo[i][j]);
+            scorekhi2 += contingence[i][j] * log(div);
         }
     }
+}
+scorekhi2  *= 2;
 
 
-    for(int j(0); j<(nbrcolonnes-1); ++j)
-    {    for(int i(0); i<(nbrligne-1); ++i)
-        {
-           contingence[j][nbrcolonnes] += contingence[i][j];
-        }
-    }
-
-
-
-    int contingencetheo[nbrcolonnes-1][nbrligne-1];
-    for(int i(0); i<(nbrcolonnes-1); ++i)
-    {    for(int j(0); j<(nbrligne-1); ++j)
-        {
-           contingencetheo[i][j] = contingence[i][nbrligne]*contingence[nbrcolonnes][j]/contingence[nbrcolonnes][nbrligne];
-        }
-    }
-
-
-
-    int scorekhi2=0;
-    for(int i(0); i<(nbrcolonnes-1); ++i)
-    {    for(int j(0); j<(nbrligne-1); ++j)
-        {
-           scorekhi2 += (contingence[i][j]-pow(contingencetheo[i][j],2))/contingence[nbrcolonnes][nbrligne];
-        }
-    }
-
-    return (scorekhi2);
+float p;
+cout<<"score: "<<scorekhi2<<endl;
+boost::math::chi_squared mydist(8);
+pval = 1 - boost::math::cdf(mydist, scorekhi2)
+p=boost::math::cdf(mydist,scorekhi2);
+cout<<"p: "<<p<<endl;
+return(scorekhi2);
 }
