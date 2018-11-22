77dc91217f23bdbd1b31405d6ccf883d240a5e74
chikitow <antoine.laine1@etu.univ-nantes.fr>
2018-11-22 12:59:50 +0100

    khi2

--- testAntoine/CSVParser.cpp
+++ testAntoine/CSVParser.cpp
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <ctime>
 #include <chrono>
+#include <boost/math/distributions/chi_squared.hpp>
 
 #include "CSVParser.hpp"
 #include "common.h"
@@ -132,6 +133,83 @@ int main()
       }
       cout<<endl;
       cout<<countNonStat<<" valeurs dans le tableau inférieures à 5."<<endl;
+
+      int const nbrcolonnes(9);
+      int const nbrligne(2);
+      //cout<<nbrcolonnes<<endl;
+
+      for(int j(0); j<(nbrcolonnes); ++j)
+      {    for(int i(0); i<(nbrligne); ++i)
+          {
+
+             //cout<<contingence[nbrligne][j]<<"+"<<contingence[i][j]<<"=";
+             contingence[nbrligne][j] += contingence[i][j];
+             //cout<<contingence[nbrligne][j]<<" ";
+
+          }
+          //cout<<";"<<endl;
+      }
+      for (int i=0;i<3;i++){
+        for (int j=0;j<10;j++){
+          cout<<contingence[i][j]<<" ";
+        }
+        cout<<endl;
+      }
+      cout<<endl;
+
+
+
+      for(int j(0); j<(nbrcolonnes); ++j)
+      {    for(int i(0); i<(nbrligne); ++i)
+          {
+             contingence[i][nbrcolonnes] += contingence[i][j];
+          }
+      }
+      for(int i(0); i<(nbrligne); ++i)
+          {
+             contingence[nbrligne][nbrcolonnes] += contingence[i][nbrcolonnes];
+          }
+      for (int i=0;i<3;i++){
+        for (int j=0;j<10;j++){
+          cout<<contingence[i][j]<<" ";
+        }
+        cout<<endl;
+      }
+      cout<<endl;
+
+
+
+      int contingencetheo[nbrcolonnes][nbrligne];
+      for(int i(0); i<(nbrligne); ++i)
+      {    for(int j(0); j<(nbrcolonnes); ++j)
+          {
+             //cout<<contingence[i][nbrcolonnes]<<"*"<<contingence[nbrligne][j]<<"/"<<contingence[nbrligne][nbrcolonnes];
+             contingencetheo[i][j] = contingence[i][nbrcolonnes]*contingence[nbrligne][j]/contingence[nbrligne][nbrcolonnes];
+            // cout<<"="<<contingencetheo[i][j]<<endl;
+          }
+      }    for (int i=0;i<2;i++){
+            for (int j=0;j<9;j++){
+              cout<<contingencetheo[i][j]<<" ";
+            }
+            cout<<endl;
+          }
+          cout<<endl;
+
+
+
+      float scorekhi2=0;
+      for(int i(0); i<(nbrligne); ++i)
+      {    for(int j(0); j<(nbrcolonnes); ++j)
+          {
+             scorekhi2 += (pow(((contingence[i][j]-(contingencetheo[i][j]))),2)/contingencetheo[i][j]);
+             //cout<<scorekhi2<<"="<<contingence[i][j]<<"-"<<contingencetheo[i][j]<<"^2"<<"/"<<contingencetheo[i][j]<<endl;
+          }
+      }
+      float p;
+      cout<<"score: "<<scorekhi2<<endl;
+      boost::math::chi_squared mydist(8);
+      p=boost::math::cdf(mydist,scorekhi2);
+      cout<<"p: "<<p<<endl;
     }
     return 0;
 }
