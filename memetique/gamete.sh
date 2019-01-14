9b7c2b3700bdc5e92ecdfab31021d315654a95b0
qclayssen <quentin.clayssen@gmail.com>
2019-01-14 16:36:37 +0100

    parsing gamete

--- memetique/gamete.sh
+++ memetique/gamete.sh
@@ -2,5 +2,5 @@
 
 
 b=$(basename $1)
-awk '{print $NF}' $1 > genotype_${b}
-awk '{$NF=""; print $0}' $1 > phenotype_${b}
+awk '{print $NF}' $1 > phenotype_${b}
+awk '{$NF=""; print $0}' $1 > genotype_${b}
