5109938cb8670cc5ba2ad3eb7d65a94368e494d6
chikitow <antoine.laine1@etu.univ-nantes.fr>
2019-02-12 19:59:48 +0100

    aaa

--- path_relinking/toy_example/launch_toy_dataset_analysis.sh
+++ path_relinking/toy_example/launch_toy_dataset_analysis.sh
@@ -10,11 +10,13 @@ simu=$(ls data | grep -i genotype)
 
 for genotype in ${simu};
 do
-  phenotype=$(echo ${genotype} | sed 's/Genotype/Phenotype/' | sed 's/genotype/phenotype/')
-  ../path_relinking data/${genotype} data/${phenotype} #Execuction of the Method
-  evalFile=$(ls ./outputs |grep ${genotype}) #First part of the evalutation : Creation of the 'results' file filed with TP/FP/FN
-  ../../eval.py outputs ${evalFile} results 2 M0P
-
-powerFile=$(ls ./results |grep ${genotype})
-../../eval_Step2.py results ${powerFile} #Second part of the evaluation : For each file
+  for i in `seq 1 10`;
+    do
+    phenotype=$(echo ${genotype} | sed 's/Genotype/Phenotype/' | sed 's/genotype/phenotype/')
+    ../path_relinking data/${genotype} data/${phenotype} #Execuction of the Method
+    evalFile=$(ls ./outputs |grep ${genotype}) #First part of the evalutation : Creation of the 'results' file filed with TP/FP/FN
+    ../../eval.py outputs ${evalFile} results 2 M0P
+    done
+  powerFile=$(ls ./results |grep ${genotype})
+  ../../eval_Step2.py results ${powerFile} #Second part of the evaluation : For each file
 done
